62e263ffa0bc0bfce5abc5c9fab47c7e2d508c12
Richard Barker <rcbarker2@wisc.edu>
2014-05-01 16:00:55 -0500

    Lost view Controller update

--- FoundIt/AppDelegate.m
+++ FoundIt/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import <Parse/Parse.h>
+#import <Parse.h>
 
 @implementation AppDelegate
 
---
+++ FoundIt/PickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PickerViewController.h
+//  FoundIt
+//
+//  Created by Richard Barker on 5/1/14.
+//  Copyright (c) 2014 Yuqian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerViewController : UITableViewController
+
+@end
